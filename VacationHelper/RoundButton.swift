579bb1bd62fc750f40fe5058ffc4de786d578fb8
Arthur Zhang <arthur@Arthurs-MacBook-Pro.local>
2017-05-28 12:56:40 -0700

    first commit

---
+++ VacationHelper/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  VacationHelper
+//
+//  Created by Arthur Zhang on 5/27/17.
+//  Copyright © 2017 Arthur Zhang. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
