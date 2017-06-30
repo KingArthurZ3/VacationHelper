57b0beea7b109092c9e31536a0d7dc3e69b2143d
Arthur Zhang <arthur@Arthurs-MacBook-Pro.local>
2017-06-29 18:47:00 -0700

    smoothed camera flying and added extra locations

--- VacationHelper/ViewController.swift
+++ VacationHelper/ViewController.swift
@@ -11,56 +11,74 @@ import GoogleMaps
 import GooglePlaces
 import GooglePlacePicker
 
+class VacationDestination: NSObject{
+    
+    let name: String?
+    let location: CLLocationCoordinate2D
+    let zoom: Float
+    
+    init(name: String, location: CLLocationCoordinate2D, zoom: Float){
+        self.name = name
+        self.location = location
+        self.zoom = zoom
+    }
+}
+
 class ViewController: UIViewController {
+    
+    var mapView: GMSMapView?
+    
+    var currentDestination: VacationDestination?
+    
+    let destinations = [VacationDestination(name: "Embarcadero Station", location: CLLocationCoordinate2DMake(37.793126, -122.397059), zoom: 15),
+                        VacationDestination(name: "Ferry Building", location: CLLocationCoordinate2DMake(37.795385, -122.393120), zoom: 18),
+                        
+                        
+                        ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-
-        let locManager = CLLocationManager()
-        locManager.requestWhenInUseAuthorization()
-        
-        var currentLocation = CLLocation()
-        
-        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
-            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways){
-            
-            currentLocation = locManager.location!
-            
-        }
-
-        
         GMSServices.provideAPIKey("AIzaSyAQO7HACrSmKRyFX8Bo_5CJQxH0Xf9M4ss")
         GMSPlacesClient.provideAPIKey("AIzaSyAQO7HACrSmKRyFX8Bo_5CJQxH0Xf9M4ss")
         
-        let camera = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, zoom: 12)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: 37.621442, longitude: -122.378888, zoom: 11)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         
         view = mapView
         
-        let currentLocationAddress = CLLocationCoordinate2DMake(currentLocation.coordinate.latitude, currentLocation.coordinate.longitude)
+        let currentLocation = CLLocationCoordinate2DMake(37.621442, -122.378888)
         
-        let marker = GMSMarker(position: currentLocationAddress)
-        marker.title = "Current Location"
-        marker.snippet = "You are here"
+        let marker = GMSMarker(position: currentLocation)
+        marker.title = "SFO Airport"
+        marker.snippet = "Start here"
         marker.map = mapView
-
+        
        
+    }
+    @IBAction func next(_ sender: UIBarButtonItem) {
         
-        // sfo airport lat: 37.621317 long: -122.379158
-        
-//        let currentLocation = CLLocationCoordinate2DMake(37.62131737, -122.379158)
-//        
-//        let marker = GMSMarker(position: currentLocation)
-//        marker.title = "SFO Airport"
-//        marker.map = mapView
-
-        
-        
+        if(currentDestination == nil){
+            currentDestination = destinations.first
+        } else {
+            if let index = destinations.index(of: currentDestination!) {
+                currentDestination = destinations[index +  1]
+            }
+        }
+        setMapCamera()
     }
  
-    
-
+    private func setMapCamera() {
+        CATransaction.begin()
+        CATransaction.setValue(2, forKey: kCATransactionAnimationDuration)
+        mapView?.animate(to: GMSCameraPosition.camera(withTarget: currentDestination!.location, zoom: currentDestination!.zoom))
+        CATransaction.commit()
+        
+        let marker = GMSMarker(position: currentDestination!.location)
+        marker.title = currentDestination?.name
+        marker.map = mapView
+        
+    }
 }
 
